1daa69dd80d2678e5fd8d7324f54b5ab17f1d2a8
gtm32 <glorialopez4578@gmail.com>
2022-10-14 21:43:21 -0600

    Agregar archivos de proyecto.

---
+++ Compra/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[]args)
+        {
+        int numerocompra;
+        double precio = 0;
+        double cantidad = 0;
+        Console.Write("Cuantos productos quiere comprar: ");
+        numerocompra = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1; i <= numerocompra; i++)
+        {
+            Console.Write("Ingrese el precio del producto numero: "+i+ " " );
+            precio = Convert.ToDouble(Console.ReadLine());
+            cantidad = cantidad + Convert.ToDouble(Console.ReadLine());
+        }
+        Console.WriteLine("El total a pagar es: " + cantidad);
+        Console.ReadLine();
+        }
+    }
