f6f95d5b6bc2e58b55265c793bddc14dee0b1d38
GloriaLopez899 <106932031+GloriaLopez899@users.noreply.github.com>
2022-10-25 22:33:17 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Ingrese la medida de los lados: ");
+double lado = double.Parse(Console.ReadLine());
+double perimetro = lado * 4;
+Console.Write($"{perimetro} ");
+Console.ReadKey();
